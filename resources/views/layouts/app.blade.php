242120662232c46af7ecd6ae52a6aceb01567553
Errami-khadija <khadiijaerrami2021@gmail.com>
2026-04-01 12:51:39 +0200

    add styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+     <link rel="stylesheet" href="{{ asset('css/userPanel_Css.css') }}">
       
     </head>
     <body class="font-sans antialiased">
